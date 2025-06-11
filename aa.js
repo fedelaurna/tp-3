05ace0778211622542e201b455bb9f21e14e87be
fedelaurna <142011660+fedelaurna@users.noreply.github.com>
2025-06-11 08:17:14 -0300

    Add files via upload

---
+++ aa.js
@@ -0,0 +1,76 @@
+/*1
+let animales = ["perro", "gato", "conejo", "loro"];
+
+for (let i = 0; i < animales.length; i++) {
+    console.log(animales[i]);
+  }
+*/
+
+  /*2
+  let animales = ["perro", "gato", "conejo", "loro"];
+
+for (let i = 0; i < animales.length; i++) {
+  if (i % 2 === 0) {
+    console.log(animales[i]);
+  }
+}
+*/
+
+/*3
+let numeros = [4, 7, 2, 9, 5];
+let suma = 0;
+
+for (let i = 0; i < numeros.length; i++) {
+  suma += numeros[i];
+}
+
+console.log("La suma es", suma);
+*/
+
+/*4
+let datos = [3, 6, 1, 8];
+let resultado= [];
+
+for (let i = 0; i < datos.length; i++) {
+    resultado.push(datos[i]*2);
+}
+
+console.log(resultado);
+*/
+/*5
+let alumnos = ["Sofía", "Carlos", "Bruno", "Lucía", "Carla"];
+
+let nombreBuscado = prompt("Ingresá un nombre para buscar:");
+
+if (alumnos.includes(nombreBuscado)) {
+  console.log("¡Nombre encontrado!");
+} else {
+  console.log("Nombre no encontrado.");
+}
+*/
+/*6
+let valores = [5, 0, 8, 0, 2];
+
+for (let i = 0; i < valores.length; i++) {
+    if (valores[i] === 0) {
+        valores[i] = 100;
+    }
+}
+
+console.log(valores); 
+
+*/
+/*
+
+let notas = [8, 7, 9, 10, 6];
+let suma = 0;
+
+
+for (let i = 0; i < notas.length; i++) {
+    suma += notas[i];
+}
+
+let promedio = suma / notas.length;
+console.log("El promedio es " + promedio); 
+
+*/
